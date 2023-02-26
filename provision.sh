7655aef9952d627d12685383c7fadb9da142a544
Miguel Reis <miguel_reis08@protonmail.com>
2023-02-26 12:38:01 -0300

    Rename script

---
+++ provision.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Adicionar repositório do Docker
+sudo apt update
+sudo apt install -y ca-certificates curl gnupg lsb-release
+
+sudo mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+      $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+sudo apt-get update
+
+# Instalar o Docker
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Criando um container do NGINX com um volume no diretório /usr/share/nginx/html, permitindo que o servidor sirva o arquivo index.html
+sudo docker container run --name viacep -dp 8080:80 -v /site:/usr/share/nginx/html:ro nginx
